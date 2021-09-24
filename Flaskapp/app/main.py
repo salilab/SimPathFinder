03409b15e202c826caa5128b23a7acf67d8d9d56
Sai Janani Ganesan <ganesans@salilab.org>
2021-09-24 14:19:00 -0700

    updated app

--- Flaskapp/app/main.py
+++ Flaskapp/app/main.py
@@ -2,18 +2,13 @@ from flask import Flask, flash, render_template, request, url_for, redirect
 import jinja2
 import os,sys
 import matplotlib.pyplot as plt
-import pandas as pd
-pd.set_option('mode.chained_assignment', None)
-import numpy as np
-import sqlite3 as sql
-from dbconnect import create_users_table,create_login_table
+#import sqlite3 as sql
+#from dbconnect import create_users_table,create_login_table
 import gensim
 import pickle
 sys.path.insert(0, "../src/pyext/")
-sys.path.append('../data/models/')
+sys.path.append('../..//models/')
 from run_server import RunServerClassifier
-from model import EnsembleClassifier,PathwayClassifier,PathwaySimilarity
-from gensim.models.fasttext import FastText as FT_gensim
 import warnings
 
 
@@ -58,29 +53,20 @@ def add_header(r):
 @app.route("/")
 @app.route("/home/")
 def home():
-    return render_template("layout.html")
+    return render_template("application.html")
 
 @app.route("/help/")
 def help():
     return render_template("help.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
 @app.route("/application/")
 def application():
-    if name and email:
-        return render_template("application.html")
-    else:
-        return "<h1>Please fill your details to proceed.</h1>"
+    return render_template("application.html")
 
 @app.route("/Similarity/",methods=['GET','POST'])
 def Similarity():
     if request.method=='POST':
         enzyme_string=request.form['list']
-        if 'name' not in list(globals().keys()) or 'email' not in list(globals().keys()):
-            return "<h1>Please login and then enter enzyme list to proceed.</h1>"
         if enzyme_string:
             num,text1=RunServerClassifier().check_format(enzyme_string)
             print (num,text1)
@@ -103,8 +89,6 @@ def Similarity():
 def Classification():
     if request.method=='POST':
         enzyme_string=request.form['list']
-        if 'name' not in list(globals().keys()) or 'email' not in list(globals().keys()):
-            return "<h1>Please login and then enter enzyme list to proceed.</h1>"
         if enzyme_string:
             num,text1=RunServerClassifier().check_format(enzyme_string)
             print (num,text1)
@@ -131,28 +115,6 @@ def Classification_Results():
 @app.route("/Similarity_Results/")
 def Similarity_Results():
     return render_template("Similarity_Results.html")
-
-@app.route('/form/', methods=['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        global name
-        name=request.form['Name']
-        global email
-        email=request.form['Email']
-        if name and email:
-            date,c, conn = create_users_table()
-            entry_exists=c.execute("SELECT EXISTS (SELECT 1 FROM users WHERE login=?) AND (SELECT 1 FROM users WHERE email=?)",(name,email)).fetchall()[0][0]
-            if entry_exists==0:
-                c.execute('INSERT INTO users (date, login, email) VALUES (?,?,?)',(date, name, email))
-                conn.commit()
-            date,c1, conn1 = create_login_table()
-            c1.execute('INSERT INTO login (date, login, email) VALUES (?,?,?)',(date, name, email))
-            conn1.commit()
-            return redirect(url_for('application'))
-        else:
-            return "<h1>Please fill your details to proceed.</h1>"
-
-    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Flaskapp/app/run_server.py
+++ Flaskapp/app/run_server.py
@@ -1,8 +1,8 @@
-from model import EnsembleClassifier,PathwayClassifier,PathwaySimilarity
 from gensim.models.fasttext import FastText as FT_gensim
 import pickle
-
+import numpy as np
 import warnings
+import pandas as pd
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 class RunServerClassifier(object):
@@ -10,30 +10,26 @@ class RunServerClassifier(object):
 
 	'''
 	def __init__(self):
-		self.path='../../data/'
-		self.models='../../data/models/'
-		self.results='../../data/results/'
-		self.fname='df_metacyc_multilabel_1.pkl'
+		self.models='../../models/'
+		self.data='../../labeldata/'
+		self.metacyc='LabeledData.pkl'
 		self.kegg='df_kegg.pkl'
-		self.model=FT_gensim.load(self.models+'model5-10-100.model')
-		self.classifier=self.results+'enspickle_model.pkl'
+		self.model=FT_gensim.load(self.models+'tierT12_5_3_5_300.model')
+		self.classifier=self.models+'RMmodel.pkl'
+		self.categories=['Detoxification','Activation',
+						'Biosynthesis', 'Degradation', 'Energy', 'Glycan',
+						'Macromolecule']
 
 
 	def check_format(self,sample):
-		sample_lower=sample.lower()
-		try:
-			sample_list=sample_lower.split(',')
-		except :
-			printed_text="Input not in the right format, you might have forgotten commas"
-			return 1,printed_text
+		sample_list=sample.lower().split(',')
 		try:
 			ec=[i.count('ec') for i in sample_list]
-			#print (ec)
-			#print (sample_list)
+
 			if len(sample_list)!=sum(ec):
 				printed_text="Input not in the right format, you might have forgotten using ec or the semicolon between ec and the numbers"
 				return 1,printed_text
-		except:
+		except (SyntaxError, ValueError):
 				printed_text= "Input not in the right format, you might have forgotten using ec or the semicolon between ec and the numbers"
 				return 1,printed_text
 		try:
@@ -44,7 +40,7 @@ class RunServerClassifier(object):
 			if True in ec_class_isal: 
 				printed_text= "Input not in the right format, your ec number has strings"
 				return 1,printed_text
-		except:
+		except (IndexError, SyntaxError, ValueError):
 				printed_text= "Input not in the right format, you might have forgotten using ec or the semicolon between ec and the numbers"
 				return 1,printed_text
 		try:
@@ -58,7 +54,7 @@ class RunServerClassifier(object):
 				printed_text= "Input not in the right format, you are not using EC classe 1-7"
 				return 1,printed_text           
         
-		except:
+		except (IndexError, SyntaxError, ValueError):
 				printed_text= "Input not in the right format, you might have forgotten using ec or the semicolon between ec and the numbers"
 				return 1,printed_text
 
@@ -66,36 +62,80 @@ class RunServerClassifier(object):
 			#check number of digits in ec numbers
 			ec_nums=set([len(i.split(':')[1].split('.')) for i in sample_list])
 			ec_nums_T=[True for i in ec_nums if i not in [2,3,4]]
-			#print ("ec_nums",ec_nums,ec_nums_T)
 			if len(ec_nums_T)>0:
 				printed_text= "Input not in the right format, check your ec numbers"
 				return 1,printed_text                
-		except:
+		except (IndexError, SyntaxError, ValueError):
 				printed_text= "Input not in the right format, check your ec numbers"
 				return 1,printed_text
 
 		return 0,"Input in the right format"
 
+	def get_pathway_vectors(self,sample_list):
+		return np.mean(self.model.wv[sample_list],axis=0)
+
 	def run_classifier(self,sample):
 		sample_list=sample.lower().split(',')
-		PW=PathwayClassifier()
 		model_gensim=self.model
-		X_test=PW.pathway_vector(sample_list,self.model).reshape(1,-1)
+		X_test=self.get_pathway_vectors(sample_list).reshape(1,-1)
 		with open(self.classifier, 'rb') as file:
 			pickle_model = pickle.load(file)
-
 		prediction_prob=pickle_model.predict_proba(X_test)
 		Y_pred = pickle_model.predict(X_test)
-		classes=[ i[0] for i in list(zip(PW.categories,Y_pred[0])) if i[1]==1 ]
-		classes_prob=[str(round(i[1],4)) for i in list(zip(PW.categories,prediction_prob[0])) if i[0] in classes]
+		classes=[ i[0] for i in list(zip(self.categories,Y_pred[0])) if i[1]==1 ]
+		classes_prob=[str(round(i[1],4)) for i in list(zip(self.categories,prediction_prob[0])) if i[0] in classes]
 		output_text1= 'The predicted class/classes: ' +' ,'.join(classes) 
 		output_text2='The predicted class probability/probabilities: ' +' ,'.join(classes_prob)
 		return output_text1,output_text2
 
+	def similarity_dict_metacyc(self):
+		data_df_multi=pickle.load(open(self.data+self.metacyc,'rb'))
+		data_Metacyc_part=data_df_multi[['Map','Name','EC']]
+		EC_list=data_Metacyc_part['EC'].to_list()
+		EC_dict={tup[0]:tup[1] for tup in list(zip(data_Metacyc_part['Map'],data_Metacyc_part['EC']))}
+		EC_name={tup[0]:tup[1] for tup in list(zip(data_Metacyc_part['Map'],data_Metacyc_part['Name']))}
+		return EC_list,EC_dict,EC_name
+
+	def similarity_dict_kegg(self):
+		data_df_multi=pickle.load(open(self.data+self.kegg,'rb'))
+		data_Metacyc_part=data_df_multi[['Map','Name','EC']]
+		EC_list=data_Metacyc_part['EC'].to_list()
+		EC_dict={tup[0]:tup[1] for tup in list(zip(data_Metacyc_part['Map'],data_Metacyc_part['EC']))}
+		EC_name={tup[0]:tup[1] for tup in list(zip(data_Metacyc_part['Map'],data_Metacyc_part['Name']))}
+		return EC_list,EC_dict,EC_name
+
+	def get_ranking_single(self,valid,metacyc=True,kegg=True):
+		
+		if metacyc:
+			data,data_dict,data_dict_name=self.similarity_dict_metacyc()
+			df_simM=pd.DataFrame(columns=['Pathway_test','Pathway_train','Similarity'])
+			for k,l in enumerate(data):
+				sim=self.model.wv.n_similarity(valid, data[k])
+				pathway=list(data_dict.keys())[k]
+				pathway_name=data_dict_name[list(data_dict.keys())[k]]
+				perc=round(sim*100,2)
+				if sim>0:
+					lst=[['UNK',list(data_dict.keys())[k],pathway_name,sim,perc]]
+					df_simM=df_simM.append(pd.DataFrame(lst,columns=['Pathway_test','Pathway_train','Pathway_name','Similarity','Percentage']))
+
+		if kegg:
+			data,data_dict,data_dict_name=self.similarity_dict_kegg()
+			df_simK=pd.DataFrame(columns=['Pathway_test','Pathway_train','Similarity'])
+			for k,l in enumerate(data):
+				sim=self.model.wv.n_similarity(valid, data[k])
+				pathway=list(data_dict.keys())[k]
+				pathway_name=data_dict_name[list(data_dict.keys())[k]]
+				perc=round(sim*100,2)
+				if sim>0:
+					lst=[['UNK',list(data_dict.keys())[k],pathway_name,sim,perc]]
+					df_simK=df_simK.append(pd.DataFrame(lst,columns=['Pathway_test','Pathway_train','Pathway_name','Similarity','Percentage']))
+		
+		return df_simM,df_simK
+
+
 	def run_similarity(self,sample):
 		sample_list=sample.lower().split(',')
-		PW=PathwaySimilarity()
-		df_simM,df_simK,df_sim=PW.get_ranking_single(sample_list)
+		df_simM,df_simK=self.get_ranking_single(valid=sample_list)
 		df_simM['Pathway Link']=df_simM['Pathway_train'].apply(lambda x:'https://biocyc.org/META/new-image?object='+str(x))
 		Metacyc=df_simM[['Pathway_train','Percentage','Pathway Link']].sort_values(by=['Percentage'],ascending=False).head().values.tolist()
 		Metacyc.insert(0,['Pathway','Percentage Similarity(%)','Pathway Link'])
@@ -106,6 +146,9 @@ class RunServerClassifier(object):
 
 if __name__=='__main__':
 	R=RunServerClassifier()
-	#print (R.check_format('ec:1.2.3.4,ec:2.3.4.44,ec:3.1.23.1'))
-	R.run_similarity('ec:1.2.3.4,ec:2.3.4.44,ec:3.1.23.1')
+	#print (R.check_format('EC:1.1.1.1'))
+	a,b=R.run_similarity('ec:1.2.3.4,ec:2.3.4.44,ec:3.1.23.1')
+	#a,b=R.run_classifier('ec:1.2.3.4,ec:2.3.4.44,ec:3.1.23.1')
+	print (a)
+	print (b)
 
